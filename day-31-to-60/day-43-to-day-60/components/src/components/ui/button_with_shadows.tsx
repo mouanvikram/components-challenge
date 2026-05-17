37153377b66debb19bb53e16ad7b832cbd08a749
mouan <mouanvikram@gmail.com>
2026-05-17 23:31:15 +0530

    added on tap scale to 0.97

--- day-31-to-60/day-43-to-day-60/components/src/components/ui/button_with_shadows.tsx
+++ day-31-to-60/day-43-to-day-60/components/src/components/ui/button_with_shadows.tsx
@@ -10,9 +10,24 @@ type ButtonWithShadow = {
 export function Button({ onClick, children }: ButtonWithShadow) {
 
     return (
-        <motion.div className="px-[2px] py-[2px] rounded-[14px] bg-linear-to-t from-[#313036] to-[#4B4951] shadow-[0px_0px_0px_1px_#0d0d0d] cursor-pointer "
+        <motion.div className="px-[2px] py-[2px] rounded-[14px] bg-linear-to-t from-[#313036] to-[#4B4951] shadow-[0px_0px_0px_1px_#0d0d0d] cursor-pointer"
+            whileTap={{
+                scale: 0.95,
+            }}
+            transition={{
+                ease: "linear",
+                duration: 0.1
+            }}
         >
-            <div className="px-6 py-2.5 bg-linear-to-b from-[#201E25] to-[#323232] rounded-[12px] text-white">{children}</div>
+            <motion.div className="px-10 py-2.5 bg-linear-to-b from-[#201E25] to-[#323232] rounded-[12px] text-gray-300"
+                whileTap={{
+                    scale: 1.02
+                }}
+
+                transition={{
+                    ease: "linear",
+                    duration: 0.1
+                }}>{children}</motion.div>
         </motion.div>
     )
 }
