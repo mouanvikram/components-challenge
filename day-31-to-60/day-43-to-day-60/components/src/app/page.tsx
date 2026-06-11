c6cca765d212663d324ffbab3f787967d4a5228a
mouan <mouanvikram@gmail.com>
2026-06-11 21:25:40 +0530

    intialized a new folder for day62-to 100

--- day-31-to-60/day-43-to-day-60/components/src/app/page.tsx
+++ day-31-to-60/day-43-to-day-60/components/src/app/page.tsx
@@ -5,7 +5,6 @@ import TransactionCards from "@/components/ui/transaction_cards";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-6 bg-gray-50">
-      <TransactionCards />
     </main>
   );
 }
---
+++ day-62-to-100/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center gap-6 bg-gray-50"></main>
+  );
+}
